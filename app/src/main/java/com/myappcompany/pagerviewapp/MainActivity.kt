fcffc9792a7156c75c197a303e6156a1ba287027
omurayuki <g1221438qz12@gmail.com>
2020-01-24 19:46:56 +0900

    MainActivityさくうせい

--- app/src/main/java/com/myappcompany/pagerviewapp/MainActivity.kt
+++ app/src/main/java/com/myappcompany/pagerviewapp/MainActivity.kt
@@ -2,11 +2,29 @@ package com.myappcompany.pagerviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.nshmura.recyclertablayout.RecyclerTabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewPager: ViewPager
+    private lateinit var pagerAdapter: MoviesPagerAdapter
+    private lateinit var recyclerTabLayout: RecyclerTabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val movies = MovieHelper.getMovieFromJson("movies.json", this)
+
+        viewPager = findViewById(R.id.viewPager)
+
+        pagerAdapter = MoviesPagerAdapter(supportFragmentManager, movies)
+        viewPager.adapter = pagerAdapter
+
+        recyclerTabLayout = findViewById(R.id.recyclerTabLayout)
+        recyclerTabLayout.setUpWithViewPager(viewPager)
+
+        viewPager.currentItem = pagerAdapter.count / 2
     }
 }
